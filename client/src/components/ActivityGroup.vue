d959ced7cbc01b1ef16991a5e5a456784c22b99b
David Harush <davidharush@gmail.com>
2023-03-20 08:05:55 +0200

    refactor activities group component

---
+++ client/src/components/ActivityGroup.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="{ 'text-align': 'left' }">
+    <div
+      v-show="index > 0"
+      :style="{ height: '15px', 'padding-inline-start': '50px' }"
+    >
+      <div
+        :style="{
+          'background-color': '#C5C5C5',
+          height: '100%',
+          width: '1px',
+        }"
+      />
+    </div>
+    <div
+      :style="{
+        'text-align': 'center',
+        padding: '2px',
+        width: '100px',
+        'border-radius': '20px',
+        'background-color': '#FCF7E1',
+      }"
+    >
+      {{ monthName }}
+    </div>
+    <Activity
+      v-for="activity in activities"
+      :key="activity.id"
+      :activity="activity"
+    >
+    </Activity>
+  </div>
+</template>
+
+<script>
+import Activity from "./Activity.vue";
+
+export default {
+  components: {
+    Activity,
+  },
+  props: ["activities", "monthName", "index"],
+};
+</script>
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,49 +1,22 @@
 <template>
   <div :style="{ margin: 'auto', width: '60%' }">
-    <div
+    <ActivityGroup
       v-for="(activities, monthName, index) in activitiesByMonth"
-      v-bind:key="monthName"
-      :style="{ 'text-align': 'left' }"
+      :activities="activities"
+      :monthName="monthName"
+      :index="index"
+      :key="monthName"
     >
-      <div
-        v-show="index > 0"
-        :style="{ height: '15px', 'padding-inline-start': '50px' }"
-      >
-        <div
-          :style="{
-            'background-color': '#C5C5C5',
-            height: '100%',
-            width: '1px',
-          }"
-        />
-      </div>
-      <div
-        :style="{
-          'text-align': 'center',
-          padding: '2px',
-          width: '100px',
-          'border-radius': '20px',
-          'background-color': '#FCF7E1',
-        }"
-      >
-        {{ monthName }}
-      </div>
-      <Activity
-        v-for="activity in activities"
-        :key="activity.id"
-        :activity="activity"
-      >
-      </Activity>
-    </div>
+    </ActivityGroup>
   </div>
 </template>
 
 <script>
-import Activity from "./Activity.vue";
+import ActivityGroup from "./ActivityGroup.vue";
 
 export default {
   components: {
-    Activity,
+    ActivityGroup,
   },
   name: "HelloWorld",
   data() {
