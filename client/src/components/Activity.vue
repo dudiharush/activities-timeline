fe7863fcbabee3a89669981c6b879829901af9b2
David Harush <davidharush@gmail.com>
2023-03-20 07:54:32 +0200

    refactor activity component

---
+++ client/src/components/Activity.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div :style="{ height: '15px', 'padding-inline-start': '50px' }">
+      <div
+        :style="{
+          'background-color': '#C5C5C5',
+          height: '100%',
+          width: '1px',
+        }"
+      />
+    </div>
+    <div
+      :style="{
+        border: '1px solid #C5C5C5',
+        padding: '20px',
+        display: 'flex',
+        'justify-content': 'space-between',
+        'align-items': 'center',
+        'border-radius': '5px',
+      }"
+    >
+      <div
+        :style="{
+          display: 'flex',
+          gap: '5x',
+          'align-items': 'center',
+          gap: '10px',
+        }"
+      >
+        <div>
+          <img
+            :src="require(`../assets/topics/${activity.iconFileName}`)"
+            width="20px"
+            height="auto"
+          />
+        </div>
+        <div>
+          <div :style="{ 'font-weight': 'bold' }">
+            {{ activity.topic_data.name }}
+          </div>
+          <div :style="{ display: 'flex' }">
+            <div>{{ activity.displayDate }}</div>
+            <div>&#8226;</div>
+            <div>{{ activity.displayTime }}</div>
+          </div>
+        </div>
+      </div>
+      <div :style="{ display: 'flex', gap: '10px' }">
+        <div>score</div>
+        <div>view work</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["activity"],
+};
+</script>
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -28,64 +28,23 @@
       >
         {{ monthName }}
       </div>
-      <div v-for="activity in activities" v-bind:key="activity.id">
-        <div :style="{ height: '15px', 'padding-inline-start': '50px' }">
-          <div
-            :style="{
-              'background-color': '#C5C5C5',
-              height: '100%',
-              width: '1px',
-            }"
-          />
-        </div>
-        <div
-          :style="{
-            border: '1px solid #C5C5C5',
-            padding: '20px',
-            display: 'flex',
-            'justify-content': 'space-between',
-            'align-items': 'center',
-            'border-radius': '5px',
-          }"
-        >
-          <div
-            :style="{
-              display: 'flex',
-              gap: '5x',
-              'align-items': 'center',
-              gap: '10px',
-            }"
-          >
-            <div>
-              <img
-                :src="require(`../assets/topics/${activity.iconFileName}`)"
-                width="20px"
-                height="auto"
-              />
-            </div>
-            <div>
-              <div :style="{ 'font-weight': 'bold' }">
-                {{ activity.topic_data.name }}
-              </div>
-              <div :style="{ display: 'flex' }">
-                <div>{{ activity.displayDate }}</div>
-                <div>&#8226;</div>
-                <div>{{ activity.displayTime }}</div>
-              </div>
-            </div>
-          </div>
-          <div :style="{ display: 'flex', gap: '10px' }">
-            <div>score</div>
-            <div>view work</div>
-          </div>
-        </div>
-      </div>
+      <Activity
+        v-for="activity in activities"
+        :key="activity.id"
+        :activity="activity"
+      >
+      </Activity>
     </div>
   </div>
 </template>
 
 <script>
+import Activity from "./Activity.vue";
+
 export default {
+  components: {
+    Activity,
+  },
   name: "HelloWorld",
   data() {
     return {
@@ -129,15 +88,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
