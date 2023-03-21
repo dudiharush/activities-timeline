4c7cd891dee74f970444c06857d27ef02b179e9b
David Harush <davidharush@gmail.com>
2023-03-21 10:08:37 +0200

    activity modal part 3

--- client/src/components/ActivityModal.vue
+++ client/src/components/ActivityModal.vue
@@ -23,6 +23,17 @@
           :time="activity.displayTime"
         />
       </div>
+      <div class="activity-details">
+        <div class="activity-comment">
+          {{ activity.comment }}
+        </div>
+        <div v-if="hasScore(activity.resource_type)">
+          <ActivityScore
+            :score="activity.score"
+            :possibleScore="activity.possible_score"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +42,8 @@
 import CloseIcon from "./CloseIcon.vue";
 import ActivityIcon from "./ActivityIcon.vue";
 import ActivityTime from "./ActivityTime.vue";
+import ActivityScore from "./ActivityScore.vue";
+import { getSettings } from "../activitySettingsMap";
 
 export default {
   props: ["activity"],
@@ -38,6 +51,12 @@ export default {
     CloseIcon,
     ActivityIcon,
     ActivityTime,
+    ActivityScore,
+  },
+  methods: {
+    hasScore: function (type) {
+      return getSettings(type).score;
+    },
   },
 };
 </script>
@@ -73,16 +92,6 @@ export default {
   justify-content: end;
 }
 
-button {
-  background-color: #ac003e;
-  width: 150px;
-  height: 40px;
-  color: white;
-  font-size: 14px;
-  border-radius: 16px;
-  margin-top: 50px;
-}
-
 .modal .activity-icon {
   height: 50px;
 }
@@ -90,5 +99,23 @@ button {
 .modal .activity-time {
   font-size: 16px;
   font-weight: 100;
+}
+
+.activity-details {
+  display: flex;
+  flex-direction: column;
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.activity-comment {
+  font-size: 40px;
+  font-weight: 500;
+}
+
+.score-element {
+  font-size: 24px;
 }
 </style>
---
+++ client/src/components/ActivityScore.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="score-element" :v-show="hasScore">
+    Score<b :style="{ 'padding-inline-start': '4px' }">{{ score }}</b
+    >/<b>{{ possibleScore }}</b>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: ["score", "possibleScore", "hasScore"],
+};
+</script>
+  
+<style scoped>
+.score-element {
+  color: #017575;
+}
+</style>
