7bdf239e6b4db138d322aa39cb8a10f84b09730c
David Harush <davidharush@gmail.com>
2023-03-22 11:48:16 +0200

    add filter by activity selection

---
+++ client/src/components/FilterByType.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="activity-types-container">
+    <div
+      v-for="(label, activityType) in this.filters"
+      :key="activityType"
+      class="activity-type"
+      :class="{ selected: isSelected(activityType) }"
+      @click="$emit('select-filter', activityType)"
+    >
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["selectedActivityTypes"],
+  data() {
+    return {
+      filters: {
+        movie: "Movies",
+        quiz: "Quiz",
+        easy_quiz: "Easy Quiz",
+        challenge: "challenge",
+        make_a_map: "Make-a-Map",
+        make_a_movie: "Make-a-Movie",
+        word_play: "Word Play",
+        related_reading: "Related Reading",
+        draw_about_it: "Draw About It",
+      },
+    };
+  },
+  methods: {
+    isSelected(activityType) {
+      return this.selectedActivityTypes.includes(activityType);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.activity-type {
+  border: 2px solid #008081;
+  color: #008081;
+  padding: 6px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 12px;
+  font-weight: bold;
+}
+
+.activity-type.selected {
+  background-color: #bdebd6;
+}
+
+.activity-types-container {
+  display: flex;
+  gap: 6px;
+  padding-bottom: 15px;
+}
+</style>
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
   <div :style="{ margin: 'auto', width: '60%' }">
+    <FilterByType
+      @select-filter="selectFilter"
+      :selectedActivityTypes="selectedActivityTypes"
+    />
     <ActivityGroup
       v-for="(activityGroup, key, index) in activityGroupsByMonth"
       :activityGroup="activityGroup"
@@ -12,29 +16,39 @@
 
 <script>
 import ActivityGroup from "./ActivityGroup.vue";
+import FilterByType from "./FilterByType.vue";
+
 import { formatActivity } from "../utils.js";
 
 export default {
   components: {
     ActivityGroup,
+    FilterByType,
   },
   name: "HelloWorld",
   data() {
     return {
       listItems: [],
+      selectedActivityTypes: [],
     };
   },
   computed: {
     activityGroupsByMonth: function () {
-      const groups = this.listItems.reduce((group, item) => {
+      const groups = this.listItems.reduce((group, activity) => {
+        if (
+          this.selectedActivityTypes.length > 0 &&
+          !this.selectedActivityTypes.includes(activity.resource_type)
+        ) {
+          return group;
+        }
         const date = new Date(0);
-        date.setUTCSeconds(item.d_created);
+        date.setUTCSeconds(activity.d_created);
         const month = date.getMonth();
         group[month] = group[month] || {
           name: date.toLocaleString("en-us", { month: "short" }),
           activities: [],
         };
-        const formattedActivity = formatActivity(item);
+        const formattedActivity = formatActivity(activity);
         group[month].activities.push(formattedActivity);
         return group;
       }, {});
@@ -51,6 +65,15 @@ export default {
       const res = await fetch("http://localhost:3000/activities/v1");
       const jsonRes = await res.json();
       this.listItems = jsonRes;
+    },
+    selectFilter(selectedActivityType) {
+      if (this.selectedActivityTypes.includes(selectedActivityType)) {
+        this.selectedActivityTypes = this.selectedActivityTypes.filter(
+          (activityType) => activityType !== selectedActivityType
+        );
+      } else {
+        this.selectedActivityTypes.push(selectedActivityType);
+      }
     },
   },
   mounted() {
