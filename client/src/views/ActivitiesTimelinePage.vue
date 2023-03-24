1197c820c8c2a0e070e1e6f724f276c4e8253d2b
David Harush <davidharush@gmail.com>
2023-03-24 13:27:39 +0300

    refactor list items fetching

---
+++ client/src/views/ActivitiesTimelinePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <ActivitiesTimeline :listItems="listItems" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ActivitiesTimeline from "@/components/ActivitiesTimeline.vue";
+
+export default {
+  components: {
+    ActivitiesTimeline,
+  },
+  methods: {
+    async fetchActivities() {
+      const res = await fetch("http://localhost:3000/activities/v1");
+      const jsonRes = await res.json();
+      this.listItems = jsonRes;
+    },
+  },
+  mounted() {
+    this.fetchActivities();
+  },
+  data() {
+    return {
+      listItems: [],
+    };
+  },
+};
+</script>
